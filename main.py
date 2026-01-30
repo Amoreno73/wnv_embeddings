1336272504328caff02da76b4aea3eb92fa367b1
Amoreno73 <113930724+Amoreno73@users.noreply.github.com>
2026-01-29 20:23:55 -0600

    all_embeddings

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import ee
-from utils import get_mean_embeddings_task
+from wnv_embeddings.utils.utils import get_mean_embeddings_task
 from config import *
 
 # will prompt you to authorize access to GEE
@@ -8,30 +8,29 @@ ee.Authenticate()
 # enter your own registered project name here
 ee.Initialize(project="wnv-embeddings")
 
+
+# =============RUN GEE TASKS============= #
 # all state fips codes available at: https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt
 
-state_fips_codes_test = ["15","16","17"]
+run_gee_tasks = False # change this to true if needed but I am avoiding the stuff below for now 
 
-# note: i temporarily removed "15", "16", "17", from this list
-# 15-17 were successful
+if run_gee_tasks == True:
+  state_fips_codes = [
+    "01", "02", "04", "05", "06", "08", "09", "10", "11", "12",
+    "13", "15", "16", "17", "18", "19", "20", "21", "22", "23",
+    "24", "25", "26", "27", "28", "29", "30", "31", "32", "33",
+    "34", "35", "36", "37", "38", "39", "40", "41", "42", "44",
+    "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", "56"
+  ]
 
-state_fips_codes = [
-  "01", "02", "04", "05", "06", "08", "09", "10", "11", "12",
-  "13", "18", "19", "20", "21", "22", "23",
-  "24", "25", "26", "27", "28", "29", "30", "31", "32", "33",
-  "34", "35", "36", "37", "38", "39", "40", "41", "42", "44",
-  "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", "56"
-]
+  for fips in state_fips_codes:
+    # users/angel314/wnv_embeddings/{save_path_here}
+    # an example save path: "users/angel314/wnv_embeddings/{fips}_2017_2024_embeddings"
+    # description is as follows: "state15_2017_2024_AlphaEarth_Embeddings"
+    save_path = f"{GEE_BASE_SAVE_PATH}{fips}_2017_2024_embeddings"
 
-# ======== 10 m resolution function ========
-for fips in state_fips_codes:
-  # users/angel314/wnv_embeddings/{save_path_here}
-  # an example save path: "users/angel314/wnv_embeddings/fips15_2017_2024_embeddings"
-  # description is as follows: "state15_2017_2024_AlphaEarth_Embeddings"
-  save_path = f"{GEE_BASE_SAVE_PATH}{fips}_2017_2024_embeddings"
+    task = get_mean_embeddings_task(fips, asset_id=save_path)
+    print(f"State {fips}: {task.status()}")
 
-  task = get_mean_embeddings_task(fips, asset_id=save_path)
-  print(f"State {fips}: {task.status()}")
-
-# began rest of tasks at 8:38 pm on 1/24/26
-# once I have ran this I go back to data_cleaning to convert this to a csv
+  # began rest of tasks at 8:38 pm on 1/24/26
+  # duration: 26 minutes for longest task, thus all the tasks finished in less than 26 minutes. 
